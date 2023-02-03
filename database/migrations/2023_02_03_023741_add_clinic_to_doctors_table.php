8cdc6db40f53cdb2afab8794b43f8c26d0dab249
leofrank <leofrankjcd_990@hotmail.com>
2023-02-02 22:40:13 -0400

    creacion de modelos

---
+++ database/migrations/2023_02_03_023741_add_clinic_to_doctors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddClinicToDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->bigInteger('clinic_id')->unsigned()->after('id');
+            $table->foreign('clinic_id')->references('id')->on('clinics');
+            
+            $table->bigInteger('schedule_id')->unsigned()->after('fullname');
+            $table->foreign('schedule_id')->references('id')->on('schedules');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            //
+        });
+    }
+}
